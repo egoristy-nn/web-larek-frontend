6f08608d5384c2948b1b3d98d84fcaf0390c6f1c
Egor Vetkin <vetkin.yegor@yandex.ru>
2024-08-15 20:20:39 +0300

    ПР 8. Проектная работа выполнена

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface IUser {
+  payment: string;
+  email: string;
+  phone: string;
+  address: string;
+  total: number;
+  items: IItem[]
+}
+
+export interface IItem {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number;
+}
+
+export interface IItemsData {
+  total: number;
+  items: IItem[];
+  preview: string | null;
+  getItem(itemId: string): IItem;
+  updateItem(item: IItem, payload: Function | null): void;
+}
+
+export interface IUserData {
+  getUserInfo(): TUserPublic;
+  setUserInfo(userData: IUser): void;
+}
+
+export type TItemPublic = Omit<IItem, 'id' | 'description'>;
+
+export type TItemModal = Omit<IItem, 'id'>;
+
+export type TItemBasket = Pick<IItem, 'title' | 'price'>;
+
+export type TUserPublic = Pick<IUser, 'items'>;
+
+export type TUserModal = Omit<IUser, 'items'>;
